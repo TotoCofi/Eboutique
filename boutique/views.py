9b29d77931031456884dc9d6cf6c509178fa402c
kdigit <dalmeidakevin910@gmail.com>
2023-06-09 13:21:07 +0200

    User add

--- boutique/views.py
+++ boutique/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-
+from .models import Roles,Users
 # Create your views here.
 
 def acceuil(request):
     return render(request,'index.html')
 
 def user(request):
-    return render(request,'user.html')
+    rol= Roles.objects.all()
+    if request.method == 'POST':
+    
+       email=request.POST.get('email')
+       user = Users.objects.filter(email=email).first()
+       if user:
+         message = "Email existe déja"
+         return render(request, 'user.html',{'error_messages': message})
+       else : 
+            nom= request.POST.get('nom')
+            prenom= request.POST.get('prenom')
+            password= request.POST.get('password')
+            c_password = request.POST.get('c_password')
+            if len(password)<8:
+                message = "Le mot de passe doit contenir au moins 8 caractères."
+                return render(request, 'user.html',{'error_messages': message})
+                    
+            if password != c_password :  
+                message = "Les mot de passe doivent etre identique"
+                return render(request, 'user.html',{'error_messages': message})
+            
+
+    
+    return render(request,'user.html',{'roles':rol})
 
 def client(request):
     return render(request,'client.html')
