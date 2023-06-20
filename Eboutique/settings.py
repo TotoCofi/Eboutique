371827a66b4e5ea09effba69449c6d2db5215923
cedric <totocofi99@gmail.com>
2023-06-20 17:08:24 +0100

    mise à jour

--- Eboutique/settings.py
+++ Eboutique/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'boutique',
-        'USER': 'root',
+        'USER': 'cedric',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
